1ebd9f270425f4950bc87f3b054e7b4cc087d663
Zeus5288 <464503223@qq.com>
2020-05-26 09:57:52 +0800

    fix:修改底部tabbar样式

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -2,8 +2,18 @@
   <div>
     <router-view></router-view>
     <van-tabbar route>
-      <van-tabbar-item replace @click="toHome" icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item replace @click="toAccount" icon="user-o">我的</van-tabbar-item>
+      <van-tabbar-item replace @click="toHome" icon="wap-home">首页</van-tabbar-item>
+      <van-tabbar-item replace @click="toHome" icon="wap-home">资讯</van-tabbar-item>
+      <van-tabbar-item class="m-main-tabbar" replace @click="toAccount">
+        <template>
+          <div>
+            <img width="35" height="35" src="../assets/img/loading.gif" />
+            <p>资源</p>
+          </div>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item replace @click="toHome" icon="wap-home">数据</van-tabbar-item>
+      <van-tabbar-item replace @click="toAccount" icon="manager">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
@@ -38,6 +48,17 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.px2rem(@name, @px) {
+  @{name}: @px / 20px * 1rem;
+}
+
+.m-main-tabbar {
+  p {
+    margin-top: 0.5rem;
+    padding-bottom: 0.6rem;
+    text-align: center;
+  }
+}
 </style>
 
