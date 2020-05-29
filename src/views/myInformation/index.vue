339015fe6f2a575d51b83f85103cc6703393f1b8
liukaining <873916840@qq.com>
2020-05-29 08:40:35 +0800

    我的信息

--- src/views/myInformation/index.vue
+++ src/views/myInformation/index.vue
@@ -103,7 +103,7 @@
             },
             //退出登录
             quitLogin(){
-                this.$toast("退出12121")
+                this.$toast("退出1212221")
                 this.$post('/public/elgrzc/loginout.do',{ticket:Cookies.get("_tcdapt_")}).then(res =>{
                     console.log(res);
                 })
